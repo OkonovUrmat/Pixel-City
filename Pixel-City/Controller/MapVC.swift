0a1c249135d373be953b5d14101cc0ae3fbc5d41
OkonovUrmat <okonov.okonov@gmail.com>
2020-05-08 20:22:18 +0600

    Initial commit

---
+++ Pixel-City/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pixel-City
+//
+//  Created by Urmat on 5/8/20.
+//  Copyright © 2020 Urmat. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
