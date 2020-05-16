9be198245f43b2d997d1e97c59a03b8aed994d34
OkonovUrmat <okonov.okonov@gmail.com>
2020-05-16 15:47:37 +0600

    MapVC: Remove spinner and label as it finished downloading

--- Pixel-City/Controller/MapVC.swift
+++ Pixel-City/Controller/MapVC.swift
@@ -94,7 +94,7 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     func addProgreesLbl() {
         progressLbl = UILabel()
         progressLbl?.frame = CGRect(x: (screenSize.width / 2) - 120, y: 175, width:  240, height: 40)
-        progressLbl?.font = UIFont(name: "Avenir Next", size: 18)
+        progressLbl?.font = UIFont(name: "Avenir Next", size: 14)
         progressLbl?.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         progressLbl?.textAlignment = .center
         collectionView?.addSubview(progressLbl!)
@@ -153,13 +153,21 @@ extension MapVC: MKMapViewDelegate {
         let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
         mapView.addAnnotation(annotation)
         
-                print(flickrUrl(forApiKey: apiKey, withAnnotation: annotation, andNumbersOfPhotos: 40))
+        print(flickrUrl(forApiKey: apiKey, withAnnotation: annotation, andNumbersOfPhotos: 40))
         
         let coordinateRegion = MKCoordinateRegion(center: touchCoordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
         
-        retrieveUrls(forAnnotation: annotation) { (true) in
-            print(self.imageUrlArray)
+        retrieveUrls(forAnnotation: annotation) { (finished) in
+            if finished {
+                self.retrieveImages { (finished) in
+                    if finished {
+                        self.removeSpinner()
+                        self.removeProgressLbl()
+                        //reload collectionView
+                    }
+                }
+            }
         }
     }
     
