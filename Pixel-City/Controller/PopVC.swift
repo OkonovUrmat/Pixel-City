c9d119630846a87ef486e93b47ad1892134fea15
OkonovUrmat <okonov.okonov@gmail.com>
2020-05-18 20:33:12 +0600

    Create PopVC

---
+++ Pixel-City/Controller/PopVC.swift
@@ -0,0 +1,18 @@
+//
+//  PopVC.swift
+//  Pixel-City
+//
+//  Created by Urmat on 5/18/20.
+//  Copyright © 2020 Urmat. All rights reserved.
+//
+
+import UIKit
+
+class PopVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
